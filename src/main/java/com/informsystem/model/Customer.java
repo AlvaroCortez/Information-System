4707a5235befe6c2b60cd50438108c4abf7130be
Denis Maslakov <maslakov_denis@mail.ru>
2016-02-16 16:21:59 +0400

    Impliment JPA/Hibernate in model classes, add HibernateSessionFactory, change names of tables, add sequences, add hibernate config file

--- src/main/java/com/informsystem/model/Customer.java
+++ src/main/java/com/informsystem/model/Customer.java
@@ -1,7 +1,99 @@
 package com.informsystem.model;
 
+import org.hibernate.annotations.Check;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Den on 15.02.16.
  */
+@Entity
+@Table(name = "CUSTOMER")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_sequence")
+    @SequenceGenerator(name = "customer_sequence", sequenceName = "customer_sequence")
+    @Column(name = "CUSTOMER_ID")
+    private Integer customerId;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<Order>();
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(name = "CUSTOMER_RESTAURANT",
+    joinColumns = @JoinColumn(name = "CUSTOMER_ID"),
+    inverseJoinColumns = @JoinColumn(name = "RESTAURANT_ID"))
+    private List<Restaurant> restaurants = new ArrayList<Restaurant>();
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "AGE")
+    private Integer age;
+
+    @Column(name = "SEX")
+    @Check(constraints = "SEX = M OR SEX = F")
+    private char sex;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
 }
--- src/main/java/com/informsystem/model/Order.java
+++ src/main/java/com/informsystem/model/Order.java
@@ -1,7 +1,72 @@
 package com.informsystem.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by Den on 15.02.16.
  */
+@Entity
+@Table(name = "ORDERS")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
+    @SequenceGenerator(name = "order_sequence", sequenceName = "order_sequence")
+    @Column(name = "ORDER_ID")
+    private Integer orderId;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "CUSTOMER_ID", foreignKey = @ForeignKey(name = "RefORDtoCUS"))
+    private Customer customer;
+
+    @Column(name = "DATE_OF_ORDER")
+    @Temporal(TemporalType.DATE)
+    private Date dateOfOrder;
+
+    @Column(name = "NUMBER_OF_PRODUCTS")
+    private Integer numberOfProducts;
+
+    @Column(name = "ORDER_AMOUNT")
+    private Integer orderAmount;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Date getDateOfOrder() {
+        return dateOfOrder;
+    }
+
+    public void setDateOfOrder(Date dateOfOrder) {
+        this.dateOfOrder = dateOfOrder;
+    }
+
+    public Integer getNumberOfProducts() {
+        return numberOfProducts;
+    }
+
+    public void setNumberOfProducts(Integer numberOfProducts) {
+        this.numberOfProducts = numberOfProducts;
+    }
+
+    public Integer getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Integer orderAmount) {
+        this.orderAmount = orderAmount;
+    }
 }
